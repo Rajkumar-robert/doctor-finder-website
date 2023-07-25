2f39d860eeb07fc34667700e02e02651fa682fbf
Rajkumar-robert <rajkumar09112003@gmail.com>
2023-07-25 23:05:52 +0530

    search page update

--- search.php
+++ search.php
@@ -17,7 +17,7 @@ $city = $_GET['city'];
 $specialty = $_GET['specialty'];
 
 // Prepare the SQL query based on the user input
-$sql = "SELECT * FROM doctors WHERE location LIKE '%$city%'";
+$sql = "SELECT * FROM doctors WHERE city LIKE '%$city%'";
 
 if (!empty($specialty)) {
   $sql .= " AND specialty = '$specialty'";
@@ -33,36 +33,40 @@ $conn->close();
 <!DOCTYPE html>
 <html>
 <head>
-  <title>Doctor 334Searching Results</title>
+  <title>Doctor Searching Results</title>
 </head>
 <body>
   <h1>Doctor Searching Results</h1>
   <p>Search Location: <?php echo $city; ?></p>
   <p>Filter Specialty: <?php echo $specialty ?: 'All'; ?></p>
   
-  <table>
-    <tr>
-      <th>Name</th>
-      <th>Specialty</th>
-      <th>Contact</th>
-      <th>address</th>
-      <th>city</th>
-      <th>Experience</th>
-      <th>Rating</th>
-      <th>Image</th>
-    </tr>
-    <?php while ($row = $result->fetch_assoc()) : ?>
+  <?php if ($result->num_rows > 0) : ?>
+    <table>
       <tr>
-        <td><?php echo $row['name']; ?></td>
-        <td><?php echo $row['specialty']; ?></td>
-        <td><?php echo $row['contact']; ?></td>
-        <td><?php echo $row['address']; ?></td>
-        <td><?php echo $row['city']; ?></td>
-        <td><?php echo $row['experience']; ?></td>
-        <td><?php echo $row['rating']; ?></td>
-        <td><img src="<?php echo $row['image']; ?>" alt="<?php echo $row['name']; ?>" height="100"></td>
+        <th>Name</th>
+        <th>Specialty</th>
+        <th>Contact</th>
+        <th>address</th>
+        <th>city</th>
+        <th>Experience</th>
+        <th>Rating</th>
+        <th>Image</th>
       </tr>
-    <?php endwhile; ?>
-  </table>
+      <?php while ($row = $result->fetch_assoc()) : ?>
+        <tr>
+          <td><?php echo $row['name']; ?></td>
+          <td><?php echo $row['specialty']; ?></td>
+          <td><?php echo $row['contact']; ?></td>
+          <td><?php echo $row['address']; ?></td>
+          <td><?php echo $row['city']; ?></td>
+          <td><?php echo $row['experience']; ?></td>
+          <td><?php echo $row['rating']; ?></td>
+          <td><img src="<?php echo $row['image']; ?>" alt="<?php echo $row['name']; ?>" height="100"></td>
+        </tr>
+      <?php endwhile; ?>
+    </table>
+  <?php else : ?>
+    <p>Not found.</p>
+  <?php endif; ?>
 </body>
 </html>

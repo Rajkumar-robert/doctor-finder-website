25e72ac00351902581b93cf4f90a6f0abb6062bb
Rajkumar-robert <rajkumar09112003@gmail.com>
2023-07-27 02:05:19 +0530

    complete

--- search.php
+++ search.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "localhost";
-$username = "id21021362_root";
-$password = "Admin@1234";
+$username = "root";
+$password = "";
 $dbname = "id21021362_doctordatabase";
 
 // Create connection
@@ -19,6 +19,7 @@ $specialty = $_GET['specialty'];
 // Prepare the SQL query based on the user input
 $sql = "SELECT * FROM doctors WHERE city LIKE '%$city%'";
 
+
 if (!empty($specialty)) {
   $sql .= " AND specialty = '$specialty'";
 }
@@ -33,40 +34,55 @@ $conn->close();
 <!DOCTYPE html>
 <html>
 <head>
-  <title>Doctor Searching Results</title>
+  <title>Doctor Seagfgjrching Results</title>
+  <link rel="stylesheet" href="style.css">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto">
+
 </head>
-<body>
+<body class="body2">
+  <div class="head">
   <h1>Doctor Searching Results</h1>
-  <p>Search Location: <?php echo $city; ?></p>
+  <button class="home-button" ><a href="index.html" >HOME</a></button>
+  </div>
+  <div Class="filter">
+    <p>Search Location: <?php echo $city; ?></p>
   <p>Filter Specialty: <?php echo $specialty ?: 'All'; ?></p>
-  
+  </div>
+  <div class="results-grid">
   <?php if ($result->num_rows > 0) : ?>
-    <table>
-      <tr>
-        <th>Name</th>
-        <th>Specialty</th>
-        <th>Contact</th>
-        <th>address</th>
-        <th>city</th>
-        <th>Experience</th>
-        <th>Rating</th>
-        <th>Image</th>
-      </tr>
       <?php while ($row = $result->fetch_assoc()) : ?>
-        <tr>
-          <td><?php echo $row['name']; ?></td>
-          <td><?php echo $row['specialty']; ?></td>
-          <td><?php echo $row['contact']; ?></td>
-          <td><?php echo $row['address']; ?></td>
-          <td><?php echo $row['city']; ?></td>
-          <td><?php echo $row['experience']; ?></td>
-          <td><?php echo $row['rating']; ?></td>
-          <td><img src="<?php echo $row['image']; ?>" alt="<?php echo $row['name']; ?>" height="100"></td>
-        </tr>
+       
+        <div class="record-container">
+          <div id="profile-pic">
+        <img src="<?php echo $row['image']; ?>" alt=""  id="profile-pic">
+        
+          </div>
+          <div class="doc-details">
+              <div id="doc-name"> <?php echo $row['name']; ?></div>  
+              <div id="speciality"><?php echo $row['specialty']; ?></div>              
+              <div id="contact"><?php echo $row['contact']; ?></div>
+              <div id="address"><?php echo $row['address']; ?>,<?php echo $row['city']; ?></div>
+              <div id="exp">Experience <?php echo $row['experience']; ?></div>
+              <div class="star-rating">
+      <?php
+        $rating = min($row['rating'], 5);
+        $fullStars = floor($rating);
+        $hasHalfStar = ($rating - $fullStars) >= 0.5;
+        for ($i = 1; $i <= $fullStars; $i++) {
+          echo '<span style="font-size:150%;color:rgb(235, 255, 0);">&starf;</span>';
+        }
+        for ($i = $fullStars + ($hasHalfStar ? 1 : 1); $i <= 5; $i++) {
+          echo '<span style="font-size:150%;color:rgb(235, 255, 0);">&star;</span>';
+        }
+      ?>
+    </div>
+    <button class="call">Call</button>
+          </div>
+        </div>
       <?php endwhile; ?>
-    </table>
   <?php else : ?>
-    <p>Not found.</p>
+    <div class="nf">Not found!</div>
   <?php endif; ?>
+  </div>
 </body>
 </html>
